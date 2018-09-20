7ffc73e65d7b7da6562ce27eebc190115af35204
mzegarski@yahoo.com <mzegarski@yahoo.com>
2018-09-20 17:55:12 -0400

    First commit for my project

---
+++ NewTestProject/src/testingYahoo/TestCore.java
@@ -0,0 +1,13 @@
+package testingYahoo;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class TestCore {
+	
+	
+
+}
